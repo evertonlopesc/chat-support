166dbb679428b00b0875039c6b5a568aa8d39028
Everton Costa <everton.lopess@outlook.com>
2021-04-20 11:40:01 -0300

    Title: Modified server.ts
    Description: Create first route get

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Hello nlw#05"
+    });
+});
+
 app.listen(3333, () => console.log("Server is running on port: 3333"));
