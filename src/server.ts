48cdfaa595fec33e3f8aa591ef713d6ad55b82ac
Everton Costa <everton.lopess@outlook.com>
2021-04-21 10:45:09 -0300

    Title: Modified server.ts
    Description: delete get and post method.

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,4 @@ import "./database";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Hello nlw#05"
-    });
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "User registered with successfully!" })
-});
-
 app.listen(3333, () => console.log("Server is running on port: 3333"));
