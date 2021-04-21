90957dc84b399398b2922858399f57c0a1028b2e
Everton Costa <everton.lopess@outlook.com>
2021-04-21 11:04:07 -0300

    Title: New file src/routes.ts
    Description: Add Router express

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "./repositories/SettingsRepository";
+
+export const routes = Router();
+
