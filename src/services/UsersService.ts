b95ae226a1b832258ffc345d25cd0980ce997f53
Everton Costa <everton.lopess@outlook.com>
2021-04-22 11:01:21 -0300

    Title: Modified UsersService.ts
    Description: Add rules, create and save methods.

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,10 +1,24 @@
-import  { getCustomRepository } from "typeorm";
-import {  } from "../repositories/UsersRepository";
-
-interface IUsersCreate {
-    email: string;
-}
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 export class UsersService {
-    
-}
+    async create( email : string) {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        });
+
+        if (userAlreadyExists) {
+            return userAlreadyExists;
+        };
+
+        const users = usersRepository.create({
+            email,
+        });
+
+        await usersRepository.save(users);
+
+        return users;
+    }
+};
