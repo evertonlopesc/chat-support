f4f27bfbfd2cca1d4a072f12efd5645811eebe6a
Everton Costa <everton.lopess@outlook.com>
2021-04-22 13:31:38 -0300

    Title: Modified Message.ts
    Description: Add atributes, Entity and ManyToOne relations.

--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -1,5 +1,33 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn, JoinColumn, ManyToOne  } from "typeorm";
 
-
+import { v4 as uuid } from "uuid";
+import { User } from "./User";
+@Entity("messages")
 export class Message {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    admin_id: string;
+
+    @Column()
+    message: string;
+    
+    @JoinColumn({ name: "user_id" })
+    @ManyToOne(() => User)
+    user: User;
+
+    @Column()
+    user_id: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
 
 }
