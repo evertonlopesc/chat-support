0fbe5286768905340d22acf6c6214a903d039f70
Everton Costa <everton.lopess@outlook.com>
2021-04-22 06:40:14 -0300

    Title: Modified SettingsService.ts
    Description: SettingsService class created for create settings.

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,5 +1,23 @@
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
 
+interface ISettingsCreate {
+    chat: boolean,
+    username: string
+};
 
-class SettingsService {
+export class SettingsService {
+    async create({ chat, username } : ISettingsCreate) {
+        const settingsRepository = getCustomRepository(SettingsRepository);
 
+        const settings = settingsRepository.create({
+            chat,
+            username
+        });
+
+        await settingsRepository.save(settings);
+
+        return settings;
+
+    };
 };
