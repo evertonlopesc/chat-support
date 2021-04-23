23e621869c9a291bd83d3911057500c3008d2d4c
Everton Costa <everton.lopess@outlook.com>
2021-04-23 06:00:59 -0300

    Title: Modified MessagesController.ts
    Description: Add resquest and response in the MessagesController class.

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -1,5 +1,18 @@
-
+import { Request, Response } from "express";
+import { MessagesService } from "../services/MessagesService";
 
 export class MessagesController {
+    async create(request: Request, response: Response) {
+        const { admin_id, text, user_id } = request.body;
 
+        const messagesService = new MessagesService();
+
+        const message = await messagesService.create({
+            admin_id,
+            text,
+            user_id,
+        });
+
+        return response.json(message);
+    }
 };
