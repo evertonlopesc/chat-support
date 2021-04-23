0772f382edd32dcece672e52a818ad10b699a832
Everton Costa <everton.lopess@outlook.com>
2021-04-23 05:38:17 -0300

    Title: New file MessagesService.ts
    Description: Add MessagesServices class with create and save.

---
+++ src/services/MessagesService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from "typeorm";
+import { MessagesRepository } from "../repositories/MessagesRepository";
+
+interface IMessagesCreate {
+    admin_id: string;
+    text: string;
+    user_id: string;
+};
+
+export class MessagesCreate {
+    async create({ admin_id, text, user_id } : IMessagesCreate) {
+        const messagesRepository = getCustomRepository(MessagesRepository);
+
+        const message = messagesRepository.create({
+            admin_id,
+            text,
+            user_id,
+        });
+
+        await messagesRepository.save(message);
+
+        return message;
+    };
+};
