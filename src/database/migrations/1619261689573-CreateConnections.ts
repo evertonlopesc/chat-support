82a0a4bbccc5b5f52c22f678d4193c530bc995b0
Everton Costa <everton.lopess@outlook.com>
2021-04-24 10:21:32 -0300

    Title: New File CreateConnections.ts
    Description: Run command yarn typeorm migration:run

---
+++ src/database/migrations/1619261689573-CreateConnections.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateConnections1619261689573 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
