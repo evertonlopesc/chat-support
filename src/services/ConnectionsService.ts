c910447c18fe9619fcf7a620828e8e03da673fd2
Everton Costa <everton.lopess@outlook.com>
2021-04-29 11:16:39 -0300

    Title: Modified ConnectionsService.ts
    Description: finds and updates added

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -3,7 +3,7 @@ import { Connection } from "../entities/Connection";
 import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
 
 interface IConnectionsCreate {
-    id?: string; // "?" optional
+    id?: string;
     admin_id?: string;
     user_id: string;
     socket_id: string;
@@ -31,8 +31,37 @@ export class ConnectionsService {
 
     async findByUserId(user_id: string) {
 
-        const connection = await this.connectionsRepository.findOne({ user_id });
+        const connection = await this.connectionsRepository.findOne({
+            user_id 
+        });
       
         return connection;
+    };
+
+    async findAllWithoutAdmin() {
+        const connection = await this.connectionsRepository.find({
+            where: {admin_id: null},
+            relations: ["user"],
+        });
+
+        return connection;
+    };
+
+    async findBySocketID(socket_id: string) {
+        const connection = await this.connectionsRepository.findOne({
+            socket_id,
+        });
+
+        return connection;
+    };
+
+    async updateAdminID(user_id: string, admin_id: string) {
+        await this.connectionsRepository
+        .createQueryBuilder()
+        .update(Connection)
+        .set({ admin_id })
+        .where("user_id = :user_id", {
+            user_id,
+        }).execute();
     };
 };
